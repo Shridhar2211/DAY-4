2fe0abf8cfbb5cd217a2ab404d6c913972a859d0
Shridhar2211 <88042097+Shridhar2211@users.noreply.github.com>
2021-09-03 17:29:52 +0530

    Add Initial Commit

---
+++ Uc1.java
@@ -0,0 +1,13 @@
+package gaming;
+
+public class Snakeladder {
+		public static void main(String[] args) 
+		{
+		      System.out.println("Welcome to SnakeAndLadder program");
+	              int position=0;
+		       System.out.println("Player is at position : "+position);
+			
+
+	}
+
+	}
